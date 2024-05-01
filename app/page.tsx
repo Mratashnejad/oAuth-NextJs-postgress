6d420e4cd9ff0a127151cb0f9a21e482305e2c6a
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-05-01 08:06:19 +0400

    first setups

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Next shadcn starter</h1>
+        <h1 className='text-3xl font-bold'>Gtnelu project </h1>
+        <h3 className='text-1xl'>"gtnelu" connects users in Armenia with top experts for their needs. Find the best professionals for any job efficiently and locally.</h3>
       </div>
     </section>
   )
