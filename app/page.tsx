70331959d0d8d4b70d8f4574a87f173f781c416d
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-05-01 14:36:37 +0400

    update data , search menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import {SearchMenu} from '@/components/searchMenu'
 
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Gtnelu project</h1>
-        <p className='text-lg'>
-          "Gtnelu" connects users in Armenia with top experts for their needs.
-          Find the best professionals for any job efficiently and locally.
-        </p>
+        <SearchMenu />
       </div>
     </section>
   );
