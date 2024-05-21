3fb712161dc774857962d59f64e712aefdbeb5b1
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-05-21 08:13:52 +0400

    update for internationalitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,41 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-import { ThemeProvider } from '@/components/theme-provider'
-import {AuthContextProvider} from '@/app/context/AuthContext'
-import './globals.css'
+// /Users/wilaliam/Documents/github/gtnelu-project/Gtnelu/app/layout.tsx
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import { ThemeProvider } from '@/components/theme-provider';
+import { AuthContextProvider } from '@/app/context/AuthContext';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata: Metadata = {
-//   title: 'Gtnelu - Find Experts Fast',
-//   description: 'Find the best professionals for any job efficiently and locally'
-// }
+const inter = Inter({ subsets: ['latin'] });
 
 interface RootLayoutProps {
-  children: React.ReactNode
-  pathname?: string // Optional pathname prop to conditionally render Header
+  children: React.ReactNode;
+  pathname?: string; 
 }
 
 export default function RootLayout({ children, pathname }: RootLayoutProps) {
-  const isDashboard = pathname === '/dashboard'
-  const isAdmin = pathname === 'Administrator-page'
+  const isDashboard = pathname === '/dashboard';
+  const isAdmin = pathname === 'Administrator-page';
 
   return (
     <html lang='en' className='scroll-smooth antialiased' suppressHydrationWarning>
       <body className={`flex min-h-screen flex-col ${inter.className}`}>
-       <AuthContextProvider>
+        <AuthContextProvider>
           <ThemeProvider
             enableSystem
             attribute='class'
             defaultTheme='system'
             disableTransitionOnChange
           >
-                {!isDashboard && <Header />}
-                
-                    <main className='flex-grow'>{children}</main>
-                <Footer />
+
+              {!isDashboard && <Header />}
+              <main className='flex-grow'>{children}</main>
+              <Footer />
           </ThemeProvider>
         </AuthContextProvider>
       </body>
     </html>
-  )
+  );
 }
