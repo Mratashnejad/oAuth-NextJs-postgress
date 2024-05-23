a8592b51be285cafb6092eb62c195ebbdcf5ff4a
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-05-23 13:40:03 +0400

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,108 +1,32 @@
-'use client'
-import    RootLayout            from '@/app/layout'
-import  { useRouter }           from 'next/navigation'
-import  { useEffect , useState} from 'react';
-import    Link                  from "next/link"
 
-//Firebase Imports////////////////////
-import  { useAuth}              from '@/app/context/AuthContext';
-//Firebase Imports////////////////////
-
-
-//UI Imports/////////////////////////
-import { CircleUser, Menu, Package2, Search } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-//UI Imports/////////////////////////
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+// Additional imports if needed
 
-import {getUserData , setUserInfoData} from '@/app/api/users/api';
-import {  UserData  } from '../../types/types';
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Example dashboard app built using the components.",
+}
 
-///FORMS
-import UserInfoForm from '@/components/forms/UserInfoForm';
-import UserAddressForm from '@/components/forms/UserAddressForm';
-import UserEmergencyContentForm from '@/components/forms/UserEmergencyContentFrom';
-
-const pathname = '/dashboard'
-
-
-export default function Dashboard() {
-  const { user, logOut } = useAuth();
-  const router = useRouter();
-  const [activeSession , setActiveSession ] = useState('userInformation')
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const [isEditing, setEditing] = useState(false);
-
-  const handleNavigation = (section : string) =>{
-    setActiveSession(section)
-  }
-  
-  const handleEditClick = () => {
-    setEditing(!isEditing);
-  };
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        if (user) {
-          const data = await getUserData(user._id);
-          setUserData(data);
-        } else {
-          setUserData(null);
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
-    fetchUserData();
-  }, [user]);
-
-  // Logout function
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      router.push('/');
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
-
-  // Save user info function
-  const handleSaveUserInfo = async (data: UserData) => {
-    try {
-      if (data) {
-        const response = await setUserInfoData(user._id, data);
-        if (response.ok) {
-          console.log('User information updated successfully');
-        } else {
-          console.error('Failed to update user information');
-        }
-      }
-    } catch (error) {
-      console.error('Error updating user information:', error);
-    }
-  };
-
+export default function DashboardPage() {
   return (
-    <RootLayout pathname={pathname}>
+    <>
       <div className="flex min-h-screen w-full flex-col">
         <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
           <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
@@ -110,7 +34,7 @@ export default function Dashboard() {
               href="#"
               className="flex items-center gap-2 text-lg font-semibold md:text-base"
             >
-              <Package2 className="h-6 w-6" />
+              {/* <Package2 className="h-6 w-6" /> */}
               <span className="sr-only">Acme Inc</span>
             </Link>
             <Link
@@ -121,14 +45,14 @@ export default function Dashboard() {
             </Link>
             <Link
               href="/dashboard/jobs"
-              className="text-muted-foreground transition-colors hover:text-foreground"
+              className="text-foreground transition-colors hover:text-foreground"
             >
               Jobs
             </Link>
 
             <Link
               href="/dashboard/settings"
-              className="text-foreground transition-colors hover:text-foreground"
+              className="text-muted-foreground transition-colors hover:text-foreground"
             >
               Settings
             </Link>
@@ -140,7 +64,7 @@ export default function Dashboard() {
                 size="icon"
                 className="shrink-0 md:hidden"
               >
-                <Menu className="h-5 w-5" />
+                {/* <Menu className="h-5 w-5" /> */}
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
@@ -150,7 +74,7 @@ export default function Dashboard() {
                   href="#"
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
-                  <Package2 className="h-6 w-6" />
+                  {/* <Package2 className="h-6 w-6" /> */}
                   <span className="sr-only">Acme Inc</span>
                 </Link>
                 <Link
@@ -182,77 +106,186 @@ export default function Dashboard() {
                 /> */}
               </div>
             </form>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="secondary" size="icon" className="rounded-full">
-                  <CircleUser className="h-5 w-5" />
-                  <span className="sr-only">Toggle user menu</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuLabel>{userData?.phoneNumber}</DropdownMenuLabel>
-
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Notification</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
           </div>
         </header>
-        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-          <div className="mx-auto grid w-full max-w-6xl gap-2">
-            <h1 className="text-3xl font-semibold">Settings</h1>
-          </div>
-          <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-            <nav
-              className="grid gap-4 text-sm text-muted-foreground"
-              x-chunk="dashboard-04-chunk-0"
-            >
-              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userInformation')}>
-                User Information
-              </Button>
-              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userAddress')}>
-                User Address
-              </Button> 
-              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userEmergencyContent')}>
-               Emergency Contact
-              </Button>
-            </nav>
-            {activeSession === 'userInformation' && (
-            <div className="grid gap-6">
-                        {userData ? (
-                          <Card>
-                            <CardHeader>
-                              <CardTitle>User Information</CardTitle>
-                              <CardDescription>Details about the User {userData.phoneNumber}</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                              <UserInfoForm userData={userData}  onSave={handleSaveUserInfo} />
-                            </CardContent>
-                            
-                          </Card>
-                          
-                        ) : (
-                          <div>Loading User data ...</div>
-                        )}
-                    </div>)}
-
-                {activeSession === 'userAddress' && (
-                  
-                  <UserAddressForm />
-                )}
-                {activeSession === 'userEmergencyContent' && (
-                  <UserEmergencyContentForm />
-                )}
-                
-                      
+        <div className="md:hidden">
+          <Image
+            src="/examples/dashboard-light.png"
+            width={1280}
+            height={866}
+            alt="Dashboard"
+            className="block dark:hidden"
+          />
+          <Image
+            src="/examples/dashboard-dark.png"
+            width={1280}
+            height={866}
+            alt="Dashboard"
+            className="hidden dark:block"
+          />
         </div>
-      </main>
-    </div>
-  </RootLayout>
-);
+        <div className="hidden flex-col md:flex">
+          <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+              {/* <TeamSwitcher /> */}
+              {/* <MainNav className="mx-6" /> */}
+              <div className="ml-auto flex items-center space-x-4">
+                {/* <Search /> */}
+                {/* <UserNav /> */}
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 space-y-4 p-8 pt-6">
+            <div className="flex items-center justify-between space-y-2">
+              <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+              <div className="flex items-center space-x-2">
+                {/* <CalendarDateRangePicker /> */}
+                <Button>Download</Button>
+              </div>
+            </div>
+            <Tabs defaultValue="overview" className="space-y-4">
+              <TabsList>
+                <TabsTrigger value="overview">Overview</TabsTrigger>
+                <TabsTrigger value="analytics" disabled>
+                  Analytics
+                </TabsTrigger>
+                <TabsTrigger value="reports" disabled>
+                  Reports
+                </TabsTrigger>
+                <TabsTrigger value="notifications" disabled>
+                  Notifications
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="overview" className="space-y-4">
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Total Revenue
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">$45,231.89</div>
+                      <p className="text-xs text-muted-foreground">
+                        +20.1% from last month
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Subscriptions
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0  0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                        <circle cx="9" cy="7" r="4" />
+                        <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">+2350</div>
+                      <p className="text-xs text-muted-foreground">
+                        +180.1% from last month
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <rect width="20" height="14" x="2" y="5" rx="2" />
+                        <path d="M2 10h20" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">+12,234</div>
+                      <p className="text-xs text-muted-foreground">
+                        +19% from last month
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Active Now
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">+573</div>
+                      <p className="text-xs text-muted-foreground">
+                        +201 since last hour
+                      </p>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                  <Card className="col-span-4">
+                    <CardHeader>
+                      <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                      {/* <Overview /> */}
+                    </CardContent>
+                  </Card>
+                  <Card className="col-span-3">
+                    <CardHeader>
+                      <CardTitle>Recent Sales</CardTitle>
+                      <CardDescription>
+                        You made 265 sales this month.
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      {/* <RecentSales /> */}
+                    </CardContent>
+                  </Card>
+                </div>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,258 @@
+'use client'
+import    RootLayout            from '@/app/layout'
+import  { useRouter }           from 'next/navigation'
+import  { useEffect , useState} from 'react';
+import    Link                  from "next/link"
+
+//Firebase Imports////////////////////
+import  { useAuth}              from '@/app/context/AuthContext';
+//Firebase Imports////////////////////
+
+
+//UI Imports/////////////////////////
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+//UI Imports/////////////////////////
+
+import {getUserData , setUserInfoData} from '@/app/api/users/api';
+import {  UserData  } from '../../types/types';
+
+///FORMS
+import UserInfoForm from '@/components/forms/UserInfoForm';
+import UserAddressForm from '@/components/forms/UserAddressForm';
+import UserEmergencyContentForm from '@/components/forms/UserEmergencyContentFrom';
+
+const pathname = '/dashboard'
+
+
+export default function Dashboard() {
+  const { user, logOut } = useAuth();
+  const router = useRouter();
+  const [activeSession , setActiveSession ] = useState('userInformation')
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [isEditing, setEditing] = useState(false);
+
+  const handleNavigation = (section : string) =>{
+    setActiveSession(section)
+  }
+  
+  const handleEditClick = () => {
+    setEditing(!isEditing);
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (user) {
+          const data = await getUserData(user._id);
+          setUserData(data);
+        } else {
+          setUserData(null);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+    fetchUserData();
+  }, [user]);
+
+  // Logout function
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      router.push('/');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
+  // Save user info function
+  const handleSaveUserInfo = async (data: UserData) => {
+    try {
+      if (data) {
+        const response = await setUserInfoData(user._id, data);
+        if (response.ok) {
+          console.log('User information updated successfully');
+        } else {
+          console.error('Failed to update user information');
+        }
+      }
+    } catch (error) {
+      console.error('Error updating user information:', error);
+    }
+  };
+
+  return (
+    <RootLayout pathname={pathname}>
+      <div className="flex min-h-screen w-full flex-col">
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+          <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            >
+              <Package2 className="h-6 w-6" />
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+            <Link
+              href="/dashboard"
+              className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Dashboard
+            </Link>
+            <Link
+              href="/dashboard/jobs"
+              className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Jobs
+            </Link>
+
+            <Link
+              href="/dashboard/settings"
+              className="text-foreground transition-colors hover:text-foreground"
+            >
+              Settings
+            </Link>
+          </nav>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="grid gap-6 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Jobs
+                </Link>
+                <Link href="#" className="hover:text-foreground">
+                  Settings
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+            <form className="ml-auto flex-1 sm:flex-initial">
+              <div className="relative">
+                {/* <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search products..."
+                  className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                /> */}
+              </div>
+            </form>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="secondary" size="icon" className="rounded-full">
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuLabel>{userData?.phoneNumber}</DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Notification</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </header>
+        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+          <div className="mx-auto grid w-full max-w-6xl gap-2">
+            <h1 className="text-3xl font-semibold">Settings</h1>
+          </div>
+          <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+            <nav
+              className="grid gap-4 text-sm text-muted-foreground"
+              x-chunk="dashboard-04-chunk-0"
+            >
+              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userInformation')}>
+                User Information
+              </Button>
+              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userAddress')}>
+                User Address
+              </Button> 
+              <Button  variant="link"  className="font-semibold text-primary" onClick={()=>handleNavigation('userEmergencyContent')}>
+               Emergency Contact
+              </Button>
+            </nav>
+            {activeSession === 'userInformation' && (
+            <div className="grid gap-6">
+                        {userData ? (
+                          <Card>
+                            <CardHeader>
+                              <CardTitle>User Information</CardTitle>
+                              <CardDescription>Details about the User {userData.phoneNumber}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                              <UserInfoForm userData={userData}  onSave={handleSaveUserInfo} />
+                            </CardContent>
+                            
+                          </Card>
+                          
+                        ) : (
+                          <div>Loading User data ...</div>
+                        )}
+                    </div>)}
+
+                {activeSession === 'userAddress' && (
+                  
+                  <UserAddressForm />
+                )}
+                {activeSession === 'userEmergencyContent' && (
+                  <UserEmergencyContentForm />
+                )}
+                
+                      
+        </div>
+      </main>
+    </div>
+  </RootLayout>
+);
+}
