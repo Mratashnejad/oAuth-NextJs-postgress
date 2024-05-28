2ed9433597c068a0ecee47cdd7c680d8beab6e6e
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-05-28 22:17:17 +0400

    token updated

--- utils/token.ts
+++ utils/token.ts
@@ -1,10 +1,23 @@
-// import jwt from 'jsonwebtoken';
+import jwt , {JwtPayload} from 'jsonwebtoken'
 
-// const SECRET_KEY = process.env.SECRET_KEY
 
-// export const generateTOken = (user) =>{
-//     return jwt.sign({uid:user.uid , phoneNumber:user.phoneNumber} , SECRET_KEY , {expiresIn : '1d'});
+const SECRET_KEY = process.env.SECRET_KEY as string
 
-// }
+export interface User {
+    uid:string;
+    phoneNumber : string;
+}
+export const generateToken = (user: User) : string =>{
+    return jwt.sign({uid:user.uid , phoneNumber:user.phoneNumber} , SECRET_KEY , {expiresIn : '1d'});
 
-// expor
+}
+
+export const decodeToken = (token: string) : User | JwtPayload | null =>{
+    try{
+        return jwt.verify(token,SECRET_KEY) as User | JwtPayload;
+        
+    }catch(error){
+        console.error ('Error decoding token' , error);
+        return null
+    }
+}
